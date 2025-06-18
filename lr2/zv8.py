8bacac1df73f5f1946c0830a92ee3b2167dd1f72
nz <64735893+nunzayin@users.noreply.github.com>
2025-06-18 09:51:35 +0300

    lr2: added relation characteristics

--- lr2/zv8.py
+++ lr2/zv8.py
@@ -5,7 +5,7 @@ from random import randint
 ANSWER_YES_ALIAS = ("yes", "y", "да", "д")
 ANSWER_NO_ALIAS = ("no", "n", "нет", "не", "н")
 RAND_ELEMS_RANGE = (-100, 100)
-RAND_GEN_TIMES = 5
+RAND_GEN_TIMES = 8
 RELATION = lambda a, b: not a % b == 0
 RELATION_DESC = "(Вариант 8) a R b = а не делится на b без остатка"
 CONSOLE_COLOR_GREEN = "\x1b[1;32;40m"
@@ -46,10 +46,10 @@ def input_set():
     return res
 
 def descartes_sqr(s):
-    return [[(s[i], s[j]) for j in range(len(s))] for i in range(len(s))]
+    return [[(a, b) for b in s] for a in s]
 
 def relation_sqr(s, R):
-    return [[R(*s[i][j]) for j in range(len(s))] for i in range(len(s))]
+    return [[R(*pair) for pair in row] for row in s]
 
 def print_square(s, ps):
     for i in range(len(s)):
@@ -71,6 +71,21 @@ def print_square(s, ps):
                     end = "")
         print("|")
 
+def is_rel_reflexive(A, R):
+    return all(R(a, a) for a in A)
+
+def is_rel_antireflexive(A, R):
+    return all(not R(a, a) for a in A)
+
+def is_rel_symmetric(A, R):
+    return all(all((not R(a, b)) or R(b, a) for b in A) for a in A)
+
+def is_rel_antisymmetric(A, R):
+    return all(all((not (R(a, b) and R(b, a))) or (a == b) for b in A) for a in A)
+
+def is_rel_transitive(A, R):
+    return all(all(all((not (R(a, b) and R(b, c))) or R(a, c) for c in A) for b in A) for a in A)
+
 def main():
     A = set()
 
@@ -89,6 +104,13 @@ def main():
         " (T - составляет отношение, F - не составляет)" if not USE_COLORS else ""
         ))
     print_square(A_sqr, R_sqr)
+
+    print("Св-ва отношения на мн-ве A:")
+    print(f"Рефлексивность: {is_rel_reflexive(A, RELATION)}")
+    print(f"Антирефлексивность: {is_rel_antireflexive(A, RELATION)}")
+    print(f"Симметричность: {is_rel_symmetric(A, RELATION)}")
+    print(f"Антисимметричность: {is_rel_antisymmetric(A, RELATION)}")
+    print(f"Рефлексивность: {is_rel_reflexive(A, RELATION)}")
 
 if __name__ == "__main__":
     main()
